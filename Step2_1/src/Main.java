67a81cf574b7fdac887458941cbe41dbffb9d1da
aleks-sh <aleks-sh@mail.ru>
2020-12-21 09:41:21 +0300

    2.2.3

--- Step2_1/src/Main.java
+++ Step2_1/src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(leapYearCount(
-                100));
+        System.out.println(charExpression(29));
     }
 
     public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
@@ -10,9 +9,9 @@ public class Main {
     }
 
     public static int leapYearCount(int year) {
-        int count =0;
-        for (int i = 1; i <=year ; i++) {
-            if(leapYear(i))
+        int count = 0;
+        for (int i = 1; i <= year; i++) {
+            if (leapYear(i))
                 count++;
         }
         return count;
@@ -20,6 +19,25 @@ public class Main {
 
     static boolean leapYear(int year) {
         return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
+    public static boolean doubleExpression(double a, double b, double c) {
+        return Math.abs(a + b - c) < 1e-4;
+    }
+
+    /**
+     * Flips one bit of the given <code>value</code>.
+     *
+     * @param value    any number
+     * @param bitIndex index of the bit to flip, 1 <= bitIndex <= 32
+     * @return new value with one bit flipped
+     */
+    public static int flipBit(int value, int bitIndex) {
+        return value ^ 1 << (bitIndex - 1);
+    }
+
+    public static char charExpression(int a) {
+        return (char) ((int) '\\' + a);
     }
 
 }
