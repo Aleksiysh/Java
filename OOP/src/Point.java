5acd9cc9930863a4775dbcb4e4739c9320dff685
Aleksey <aleks-sh@mail.ru>
2020-12-23 13:24:59 +0300

    new file:   .idea/uiDesigner.xml

---
+++ OOP/src/Point.java
@@ -0,0 +1,22 @@
+public class Point {
+    private final double x;
+    private final double y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+}
