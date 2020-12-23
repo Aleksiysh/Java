c24949ef2baab9eef1cbf43046d63d2ab167157d
Aleksey <aleks-sh@mail.ru>
2020-12-23 13:49:03 +0300

    ССылка на метод

--- OOP/src/OOP.java
+++ OOP/src/OOP.java
@@ -5,12 +5,12 @@ public class OOP {
     public static void main(String[] args) {
 
         Timer timer = new Timer();
-        long time = timer.measureTime(
-                () -> {
-                    new BigDecimal("1234567").pow(100000);
-                }
-        );
+        long time = timer.measureTime(OOP::bigDecimalPower);
         System.out.println(time);
+    }
+
+    private static void bigDecimalPower() {
+        new BigDecimal("1234567").pow(100000);
     }
 
 //    private static class BigDecimalPower implements Runnable{
