7bb6d22b5e741798bc8a8eed90e71e1a4fadc932
Aleksey <aleks-sh@mail.ru>
2020-12-23 13:44:33 +0300

    Lambda

--- OOP/src/OOP.java
+++ OOP/src/OOP.java
@@ -5,12 +5,11 @@ public class OOP {
     public static void main(String[] args) {
 
         Timer timer = new Timer();
-        long time = timer.measureTime(new Runnable() {
-            @Override
-            public void run() {
-                new BigDecimal("1234567").pow(100000);
-            }
-        });
+        long time = timer.measureTime(
+                () -> {
+                    new BigDecimal("1234567").pow(100000);
+                }
+        );
         System.out.println(time);
     }
 
