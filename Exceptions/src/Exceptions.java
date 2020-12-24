093694fc3f4ee70bed936e4a54c4ce11c50288ca
aleks-sh <aleks-sh@mail.ru>
2020-12-24 10:03:47 +0300

    4.1.10

--- Exceptions/src/Exceptions.java
+++ Exceptions/src/Exceptions.java
@@ -1,5 +1,30 @@
 public class Exceptions {
     public static void main(String[] args) {
+        System.out.println(getCallerClassAndMethodName());
+        anotherMethod();
+    }
 
+    public static double sqrt(double x) {
+        if (x < 0)
+            throw new IllegalArgumentException("Expected non-negative number, got " + x);
+        return Math.sqrt(x); // your implementation here
+    }
+
+    private static void anotherMethod() {
+        System.out.println(getCallerClassAndMethodName());
+    }
+
+    public static String getCallerClassAndMethodName() {
+
+        Throwable throwable = new Throwable();
+        if (throwable.getStackTrace().length > 2) {
+            StackTraceElement stackTraceElement = throwable.getStackTrace()[2];
+            String className = stackTraceElement.getClassName();
+            String methodName = stackTraceElement.getMethodName();
+            return className+"#"+methodName; // your implementation here
+        }
+        return null;
     }
 }
+
+
