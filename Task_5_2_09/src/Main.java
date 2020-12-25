f3098af04829aed6441925f98a4387bf7c4dfb3e
Aleksey <aleks-sh@mail.ru>
2020-12-25 12:36:31 +0300

    5.3.13

--- Task_5_2_09/src/Main.java
+++ Task_5_2_09/src/Main.java
@@ -1,7 +1,18 @@
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Charset charset = StandardCharsets.US_ASCII;
+
+        byte[] b = new byte[]{48, 49, 50, 51};
+        InputStream inputStream = new ByteArrayInputStream(b);
+        String str = readAsString(inputStream, charset);
+        System.out.println(str);
+
+        System.exit(0);
+
         int next = 0;
         int prev = System.in.read();
         while (prev != -1) {
@@ -13,4 +24,15 @@ public class Main {
         }
         System.out.flush();
     }
+
+    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
+        Reader reader = new InputStreamReader(inputStream, charset);
+        int i;
+        StringBuilder str = new StringBuilder();
+        while ((i = reader.read()) != -1) {
+            str.append((char) i);
+        }
+        return str.toString();
+    }
+
 }
