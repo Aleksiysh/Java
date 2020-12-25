8bcaf4f96c85144c7ad999f8b2134cdfeed1073c
Aleksey <aleks-sh@mail.ru>
2020-12-25 04:41:50 +0300

    Logging

--- Logging/src/LoggingClass.java
+++ Logging/src/LoggingClass.java
@@ -14,6 +14,7 @@ public class LoggingClass {
             System.exit(2);
         }
         LOGGER.fine("Finished successfully");
+
     }
 
     private static void randomlyFailingAlgorithm() {
@@ -22,5 +23,32 @@ public class LoggingClass {
         if (randomNumber < 0.5) {
             throw new IllegalStateException("Invalid phase of the Moon");
         }
+    }
+
+    private static void configureLogging() {
+        // your implementation here
+        java.util.logging.Logger logger;
+
+        //Логгер с именем "org.stepic.java.logging.ClassA" принимал сообщения всех уровней.
+        logger = java.util.logging.Logger.getLogger("org.stepic.java.logging.ClassA");
+        logger.setLevel(Level.ALL);
+
+        //Логгер с именем "org.stepic.java.logging.ClassB" принимал только сообщения уровня WARNING и серьезнее.
+        logger = java.util.logging.Logger.getLogger("org.stepic.java.logging.ClassB");
+        logger.setLevel(Level.WARNING);
+
+        //Сообщения от всех логгеров с именами, начинающимися на "org.stepic.java"
+        //независимо от уровня печатались в консоль в формате XML
+        java.util.logging.ConsoleHandler handler =
+                new java.util.logging.ConsoleHandler();
+        handler.setFormatter(new java.util.logging.XMLFormatter());
+        handler.setLevel(Level.ALL);
+
+        logger = java.util.logging.Logger.getLogger("org.stepic.java");
+        logger.addHandler(handler);
+
+        //Сообщения от всех логгеров с именами, начинающимися на "org.stepic.java",
+        //не передавались вышестоящим обработчикам ("org.stepic", "org" и "").
+        logger.setUseParentHandlers(false);
     }
 }
