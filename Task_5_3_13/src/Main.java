c44d5fbfae920074f77e59dcbe00cd8afa17b57e
Aleksey <aleks-sh@mail.ru>
2020-12-25 13:05:21 +0300

    5.3.13

--- Task_5_3_13/src/Main.java
+++ Task_5_3_13/src/Main.java
@@ -1,5 +1,23 @@
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.Scanner;
+
+import static java.lang.Double.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("5.3.13");
+        double d = 0;
+        Reader reader = new StringReader(
+                "-1e3\n" +
+                        "18 .111 11bbb");
+        // Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(reader);
+        while (scanner.hasNext()) {
+            try {
+                d += Double.parseDouble(scanner.next());
+            } catch (java.lang.NumberFormatException e) {
+            }
+        }
+        System.out.printf("%.6f", d);
     }
 }
